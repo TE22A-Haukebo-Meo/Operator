db4f70c61b2cc571470e5b12a71400fcc8cce00c
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-10-04 10:38:14 +0200

    Tror jag gjort rätt

--- src/App.java
+++ src/App.java
@@ -16,5 +16,27 @@ public class App {
         System.out.println(fält[0]==fält[2]);
         System.out.println(fält[0]<fält[1]);
         System.out.println(fält[0]>fält[2]);
+
+        System.out.println("Uno mas tal plz");
+            tb.nextLine();
+        int x = tb.nextInt();
+        System.out.println("Addera 7");
+        x=x+7;
+        System.out.println(x);
+        System.out.println("Multiplicera med 2");
+        x=x*2;
+        System.out.println(x);
+        System.out.println("Subtrahera 6");
+        x=x-6;
+        System.out.println(x);
+        System.out.println("Halvera");
+        x=x/2;
+        System.out.println(x);
+        System.out.println("Addera 3");
+        x=x+3;
+        System.out.println(x);
+        System.out.println("Subtrahera ditt ursprungliga tal");
+        x=x-7;
+        System.out.println("Svaret är 7!");
     }
 }
